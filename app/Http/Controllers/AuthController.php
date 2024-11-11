a8472f4d76e2b9bb5a6672f9af04b7208dfaaf1d
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-11 21:59:48 +0700

    authenticate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    protected $directory = 'auth';
+
+    public function index()
+    {
+        $this->data['title'] = 'Login';
+        return $this->render('index');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'     => ['required', 'email'],
+            'password'  => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
 }
