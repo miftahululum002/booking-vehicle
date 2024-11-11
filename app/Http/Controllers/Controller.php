8a17347c5760af62b0c0b2df7fe2986117a13868
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-11 21:37:28 +0700

    base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,23 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected $data = [];
+    protected $directory = null;
+
+    protected function render($filename)
+    {
+        $this->setData();
+        return view($this->getView($filename), $this->data);
+    }
+
+    private function setData()
+    {
+        $app = getApp();
+        $this->data['app_name'] = $app->name;
+    }
+
+    private function getView($filename)
+    {
+        return !empty($this->directory) ? $this->directory . '.' . $filename : $filename;
+    }
 }
