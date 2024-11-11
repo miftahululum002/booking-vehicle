b59e4e5f7e5b778a21921ad7c1bb641ea5a0550f
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-11 21:35:16 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/', 'index')->name('login');
 });
+
+// Route::middleware(['auth'])->group(function () {
+Route::name('dashboard.')->group(function () {
+    Route::prefix('dashboard')->group(function () {
+        Route::controller(DashboardController::class)->group(function () {
+            Route::get('/', 'index')->name('index');
+        });
+    });
+});
+// });
