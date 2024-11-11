875d2c038a93d24e31827bff8aa0315698a1cb29
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-12 06:56:22 +0700

    route for log out

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::get('/', 'index')->name('login');
     Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/logout', 'logout')->name('logout');
 });
 
 Route::middleware(['auth'])->group(function () {
