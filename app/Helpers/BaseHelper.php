48d7f7d245376ebc7046672bf27b2c9aa6d3b7f6
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-11 22:08:38 +0700

    helper

--- app/Helpers/BaseHelper.php
+++ app/Helpers/BaseHelper.php
@@ -1,4 +1,7 @@
 <?php
+
+use App\Models\ActivityLog;
+
 function getApp()
 {
     return (object) config('miftahululum.app');
@@ -7,4 +10,48 @@ function getApp()
 function getAuthor()
 {
     return (object) config('miftahululum.author');
+}
+
+function getClientIp()
+{
+    return request()->ip();
+}
+
+function getUserLoginId()
+{
+    $user = getUserLogin();
+    if ($user) {
+        return $user->id;
+    }
+    return;
+}
+
+function getUserAgent()
+{
+    return request()->header('user-agent');
+}
+
+function getUserLogin()
+{
+    return auth()->user();
+}
+
+function createActivityLog($activity, $data = null, $userId = null)
+{
+    if (empty($userId)) {
+        $userId = getUserLoginId();
+    }
+    $object = [
+        'activity' => $activity,
+        'user_id' => $userId,
+        'created_by' => $userId,
+        'page_url' => url()->current(),
+        'ip_address' => getClientIp(),
+        'user_agent' => getUserAgent(),
+    ];
+
+    if (!empty($data)) {
+        $object['data'] = json_encode($data);
+    }
+    return ActivityLog::create($object);
 }
