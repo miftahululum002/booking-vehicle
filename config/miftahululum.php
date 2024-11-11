45d453a85233e41e7eb0d21a97a2bf3a9317e7c7
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-11 21:40:18 +0700

    config

--- config/miftahululum.php
+++ config/miftahululum.php
@@ -3,4 +3,8 @@ return [
     'app' => [
         'name' => 'Booking Vehicle'
     ],
+    'author' => [
+        'name' => 'Miftahul Ulum',
+        'email' => 'ulumiftahul06@gmail.com',
+    ],
 ];
