f99f752060d17b5202b66b8f4dc2875eef142917
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-12 09:39:19 +0700

    base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,8 +13,15 @@ abstract class Controller
         return view($this->getView($filename), $this->data);
     }
 
+    protected function renderDatatable($filename, $datatable)
+    {
+        $this->setData();
+        return $datatable->render($this->getView($filename), $this->data);
+    }
+
     private function setData()
     {
+        $this->data['title'] = !empty($this->data['title']) ? $this->data['title'] : 'Index';
         $app = getApp();
         $this->data['app_name'] = $app->name;
         $author = getAuthor();
