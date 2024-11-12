81e0b330d60e1e21efffe259de6571abdbcde230
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-12 11:09:15 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +20,14 @@ Route::middleware(['auth'])->group(function () {
         Route::prefix('dashboard')->group(function () {
             Route::controller(DashboardController::class)->group(function () {
                 Route::get('/', 'index')->name('index');
+            });
+            Route::name('bookings.')->group(function () {
+                Route::prefix('bookings')->group(function () {
+                    Route::controller(BookingController::class)->group(function () {
+                        Route::get('/', 'index')->name('index');
+                        Route::get('/create', 'create')->name('create');
+                    });
+                });
             });
             Route::name('vehicles.')->group(function () {
                 Route::prefix('vehicles')->group(function () {
