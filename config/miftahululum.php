37ccbbad53390aca7c3b7eab03c386eeb72dc73c
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-12 09:38:38 +0700

    config

--- config/miftahululum.php
+++ config/miftahululum.php
@@ -7,4 +7,13 @@ return [
         'name' => 'Miftahul Ulum',
         'email' => 'ulumiftahul06@gmail.com',
     ],
+    'table' => [
+        'id' => 'table-data',
+        'page_length' => '25',
+        'class' => 'table-hover table-stripped table-full',
+    ],
+    'code' => [
+        'pad' => 4,
+        'vehicle' => 'V',
+    ],
 ];
